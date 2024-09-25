38e9bf1c4e91daed98663a47b498786e2e618663
Your Name <you@example.com>
2024-09-25 14:37:24 +0200

    '1'

--- Selenium6_POM.py
+++ Selenium6_POM.py
@@ -1,5 +1,6 @@
 # Page Object Model
 import time
+import datetime
 
 from selenium.webdriver.common.by import By
 
@@ -17,19 +18,30 @@ class LoginPage:
         print('Nazwa strony', self.driver.title)
         print('adres:', self.driver.current_url)
 
+    def get_page_url(self):
+        return self.driver.current_url
+
     def enter_username(self, username):
+        time.sleep(1)
         field = self.driver.find_element(By.ID, self.username_field_id)
         field.clear()
         field.send_keys(username)
 
     def enter_password(self, password):
+        time.sleep(1)
         field = self.driver.find_element(By.ID, self.password_field_id)
         field.clear()
         field.send_keys(password)
 
     def click_login(self):
-        button = self.driver.find_element(By.NAME, self.login_button_name)
+        button = self.driver.find_element(By.NAME, self.login_btn_name)
         button.click()
+
+    def make_screenshot(self, name='screenshot'):
+        teraz = datetime.datetime.now()
+        file_name = teraz.strftime(str(name)+'_%H-%M-%S.png')
+        self.driver.get_screenshot_as_file(file_name)
+
 
     def close(self):
         self.driver.quit()
--- Selenium7_kod_do_POM.py
+++ Selenium7_kod_do_POM.py
@@ -6,12 +6,24 @@ from Selenium2 import make_screenshot
 from selenium.webdriver.common.by import By
 from time import sleep
 
+addr_after_login = 'https://www.saucedemo.com/inventory.html'
+# id="inventory_container"
+
 driver = webdriver.Chrome()
 page = LoginPage(driver)
 page.open()
 page.print_page_info()
-page.enter_username('Kamil')
+page.enter_username('standard_userA')
+page.enter_password('secret_sauce')
+page.click_login()
+sleep(2)
+
+try:
+    assert page.get_page_url() == addr_after_login
+except AssertionError:
 
 
+
+page.make_screenshot('SKRIIIN')
 page.close()
 
